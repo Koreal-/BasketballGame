949bbc471e7c76ce052e3531d13f2051d3d05e03
Kartinin Studio <dmitrykartinin@gmail.com>
2021-01-26 13:12:38 +0300

    Update screen

--- BasketballGame/ViewController.swift
+++ BasketballGame/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             configuration.planeDetection = []
             sceneView.session.run(configuration, options: .removeExistingAnchors)
         }
+        
     }
     
     //MARK: -
@@ -69,22 +70,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Ball node
         let ballNode = SCNNode(geometry: ball)
         
+        //Add physics body
+        ballNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: ballNode))
+        
         // Calculate force for pushing the ball
-        let power = Float(5)
+        let power = Float(10)
         let x = -matrixCameraTransform.m31 * power
         let y = -matrixCameraTransform.m32 * power
         let z = -matrixCameraTransform.m33 * power
         let forceDirection = SCNVector3(x,y,z)
-        
-        //Add physics body
-        ballNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: ballNode))
         
         //Apply force
         ballNode.physicsBody?.applyForce(forceDirection, asImpulse: true)
         
         
         // Assign camera position to ball
-        ballNode.simdTransform = frame.camera.transform
+        ballNode.simdTransform = cameraTransform
         
         return ballNode
     }
@@ -134,26 +135,104 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         plane.height = CGFloat(extent.z)
     }
     
-    // MARK: - ARSCNViewDelegate
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        guard let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .vertical else {
+    func getFloorNode(for anchor: ARPlaneAnchor) -> SCNNode {
+        let extent = anchor.extent
+        let floor = SCNPlane(width: CGFloat(extent.x), height: CGFloat(extent.y))
+        floor.firstMaterial?.diffuse.contents = UIColor.yellow
+        
+        let floorNode = SCNNode(geometry: floor)
+        floorNode.opacity = 0.75
+        
+        return floorNode
+    }
+    
+    func updateFloorNode(_ node: SCNNode, for anchor: ARPlaneAnchor) {
+        guard let floorNode = node.childNodes.first, let floor = floorNode.geometry as? SCNPlane else {
             return
         }
+        
+        //Change plane node center
+        floorNode.simdPosition = anchor.center
+        
+        //Change plane size
+        let extent = anchor.extent
+        floor.width = CGFloat(extent.x)
+        floor.height = CGFloat(extent.y)
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor//, planeAnchor.alignment == .vertical
+        else {
+            return
+        }
+        let width1 = CGFloat(planeAnchor.extent.x)
+        let height1 = CGFloat(planeAnchor.extent.z)
+        let plane = SCNPlane(width: width1, height: height1)
+        
+        plane.firstMaterial?.diffuse.contents = UIColor.green
+        
+        // Create 25% transparent
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.opacity = 0.25
+        
+        let x1 = CGFloat(planeAnchor.center.x)
+        let y1 = CGFloat(planeAnchor.center.y)
+        let z1 = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x1,y1,z1)
+        planeNode.eulerAngles.x = -.pi / 2
+        
+        // 6
+        node.addChildNode(planeNode)
+        /*guard let floorAnchor = anchor as? ARPlaneAnchor, floorAnchor.alignment == .horizontal else {
+         return
+         }*/
         // Add the hoop to the center of detected vertical plane
-        node.addChildNode(getPlaneNode(for: planeAnchor))
+        //node.addChildNode(getPlaneNode(for: planeAnchor))
+        
+        //node.addChildNode(getFloorNode(for: floorAnchor))
+        //guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        // 2
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        let floor = SCNPlane(width: width, height: height)
+        
+        // 3
+        floor.materials.first?.diffuse.contents = UIColor.blue
+        
+        // 4
+        let floorNode = SCNNode(geometry: floor)
+        
+        // 5
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        floorNode.position = SCNVector3(x,y,z)
+        floorNode.eulerAngles.x = -.pi / 2
+        
+        // 6
+        node.addChildNode(floorNode)
+        
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .vertical else {
             return
         }
+        /*guard let floorAnchor = anchor as? ARPlaneAnchor, floorAnchor.alignment == .horizontal else {
+         return
+         }*/
         
         //Update plane node
         updatePlaneNode(node, for: planeAnchor)
+        
+        //updateFloorNode(node, for: floorAnchor)
     }
     
     //MARK: - ACTIONS
     @IBAction func userTapped(_ sender: UITapGestureRecognizer) {
+        
         if isHoopAdded {
             //Add balls
             guard let ballNode = getBall() else { return }
@@ -162,7 +241,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         } else {
             let location = sender.location(in: sceneView)
             
-            guard let result = sceneView.hitTest(location, types: .estimatedVerticalPlane).first else {
+            guard let result = sceneView.hitTest(location, types: .existingPlaneUsingExtent).first else {
                 return
             }
             
@@ -177,7 +256,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             // Rotate hoop by 90
             hoopNode.eulerAngles.x -= .pi / 2
-            
+            isHoopAdded = true
             sceneView.scene.rootNode.addChildNode(hoopNode)
         }
         
